ae7bf5913ae4e1c58c69703f7ce50fc58a82bc51
André Coelho <dedeco99@gmail.com>
2020-02-05 17:11:18 +0000

    Added _created to log

--- models/log.js
+++ models/log.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const LogSchema = new Schema({
 	status: { type: String, default: "" },
 	data: { type: Object, default: null },
-});
+}, { timestamps: { createdAt: "_created", updatedAt: "_modified" } });
 
 const Log = mongoose.model("Log", LogSchema);
 
