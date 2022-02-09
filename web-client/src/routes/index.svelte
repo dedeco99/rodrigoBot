78acb3331374879a0d15435f622e1deac3840a1a
dedeco99 <dedeco99@gmail.com>
2022-02-09 19:29:35 +0000

    Added command parameters to prompt

--- web-client/src/routes/index.svelte
+++ web-client/src/routes/index.svelte
@@ -12,72 +12,104 @@
 	import Youtube from "$lib/youtube.svelte";
 
 	let prompt = "";
+	let command = null;
+	let options = {};
 	let chat = [];
 
 	const commands = {
-		define: { regex: /(?<word>.+)/, options: ["word"], component: Define },
-		search: { regex: /(?<word>.+)/, options: ["word"], component: Search },
-		sort: { regex: /(?<values>.+)/, options: ["values"], component: Sort },
-		math: { regex: /(?<expression>.+)/, options: ["expression"], component: Math },
-		weather: { regex: /(?<location>.+)/, options: ["location"], component: Weather },
+		define: { name: "define", options: ["word"], component: Define },
+		search: { name: "search", options: ["word"], component: Search },
+		sort: { name: "sort", options: ["values"], component: Sort },
+		math: { name: "math", options: ["expression"], component: Math },
+		weather: { name: "weather", options: ["location"], component: Weather },
 
-		convert: {
-			regex: /(?<number>[0-9]+) (?<from>\w+) (?<to>\w+)/,
-			options: ["number", "from", "to"],
-			component: Convert,
-		},
-		crypto: { regex: /(?<symbol>.+)/, options: ["symbol"], component: Crypto },
-		stock: { regex: /(?<symbol>.+)/, options: ["symbol"], component: Stock },
+		convert: { name: "convert", options: ["number", "from", "to"], component: Convert },
+		crypto: { name: "crypto", options: ["symbol"], component: Crypto },
+		stock: { name: "stock", options: ["symbol"], component: Stock },
 
-		instagram: { regex: /(?<handle>.+)/, options: ["handle"], component: Instagram },
-		reddit: { regex: /(?<subreddit>.+)/, options: ["subreddit"], component: Reddit },
-		youtube: { regex: /(?<channel>.+)/, options: ["channel"], component: Youtube },
+		instagram: { name: "instagram", options: ["handle"], component: Instagram },
+		reddit: { name: "reddit", options: ["subreddit"], component: Reddit },
+		youtube: { name: "youtube", options: ["channel"], component: Youtube },
 	};
 
-	async function sendCommand(e) {
-		e.preventDefault();
-
-		const regex = /(?<isCommand>\/)(?<command>\w+) (?<options>.+)/;
+	function handleInput() {
+		const regex = /(?<isCommand>\/)(?<command>\w+)/;
 
 		const match = prompt.match(regex);
 
-		if (match) {
-			const command = match.groups.command;
-			const options = match.groups.options.match(commands[command].regex);
+		if (match && commands[match.groups.command]) {
+			command = commands[match.groups.command];
+		} else {
+			command = null;
+			options = {};
+		}
+	}
 
-			const validatedOptions = {};
-			for (let i = 0; i < commands[command].options.length; i++) {
-				validatedOptions[commands[command].options[i]] = options.groups[commands[command].options[i]];
-			}
+	async function sendCommand(e) {
+		if (!command) return;
 
-			const res = await fetch(`http://localhost:5000/api/commands/${command}`, {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify(validatedOptions),
-			});
+		for (let i = 0; i < command.options.length; i++) {
+			if (!options[command.options[i]]) return alert("400 Bad Request");
+		}
 
-			const json = await res.json();
+		const res = await fetch(`http://localhost:5000/api/commands/${command.name}`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(options),
+		});
 
-			if (res.status === 200) {
-				chat = [{ command, data: json.data }, ...chat];
-			} else {
-				// TODO: translate message
-				chat = [json.message, ...chat];
-			}
+		const json = await res.json();
 
-			prompt = "";
+		if (res.status === 200) {
+			chat = [{ command: command.name, data: json.data }, ...chat];
+		} else {
+			// TODO: translate message
+			chat = [json.message, ...chat];
+		}
+
+		prompt = "";
+		command = null;
+		options = {};
+	}
+
+	function preventDefault(e) {
+		e.preventDefault();
+	}
+
+	function handleKeypress(e) {
+		preventDefault(e);
+
+		if (e.key === "Enter") {
+			sendCommand(e);
+		} else {
+			handleInput();
 		}
 	}
 </script>
 
 <div>
-	<form on:submit={sendCommand}>
-		<input class="prompt" type="text" bind:value={prompt} />
+	<form on:submit={preventDefault} on:keyup={handleKeypress}>
+		<input
+			class="prompt"
+			type="text"
+			bind:value={prompt}
+			style="--width: {prompt.length ? `${prompt.length + 0.5}ch` : '100%'}"
+		/>
+		{#if command}
+			{#each command.options as option}
+				<label for={option}>{option}:</label>
+				<input class="option" id={option} bind:value={options[option]} />
+			{/each}
+		{/if}
 	</form>
 	<div class="chat">
 		{#each chat as message}
 			<div class="message">
-				<svelte:component this={commands[message.command].component} data={message.data} />
+				{#if typeof message === "string"}
+					{message}
+				{:else}
+					<svelte:component this={commands[message.command].component} data={message.data} />
+				{/if}
 			</div>
 		{/each}
 	</div>
@@ -85,18 +117,40 @@
 </div>
 
 <style lang="scss">
-	.prompt {
-		width: 500px;
-		height: 50px;
+	form {
+		width: 600px;
+		display: flex;
+		align-items: center;
 		background: #444;
 		border-radius: 5px;
-		border: 0px;
-		color: white;
-		font-size: 2em;
+
+		input {
+			height: 30px;
+			background: none;
+			color: white;
+			border: none;
+			font-size: 1.25em;
+			font-family: "Source Code Pro", monospace;
+			margin: 10px;
+		}
+
+		.prompt {
+			width: var(--width);
+		}
+
+		label {
+			background: #222;
+			padding: 2px 10px;
+			border-radius: 5px;
+		}
+
+		.option {
+			width: 100%;
+		}
 	}
 
 	.chat {
-		width: 500px;
+		width: 600px;
 		height: calc(100% - 60px);
 		background: #444;
 		border-radius: 5px;
